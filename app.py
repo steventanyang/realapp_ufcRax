a965c231337ae9afec6415649fb7abd904b58d3a
stevenyang <steventanyang@gmail.com>
2024-03-13 21:37:35 -0400

    added compare feature

--- app.py
+++ app.py
@@ -17,14 +17,24 @@ st.markdown("""<br><br>""", unsafe_allow_html=True)
 
 df = pd.read_csv("final_values.csv")
 
-# Search bar
-search_query = st.text_input("Search by fighter name:", "")
 
-num_rows_to_display = st.selectbox("Number of rows to display:", options=[10, 20, 30, 40, 50, 100], index=2)  # Default to 30
+left_column, right_column = st.columns([2, 2])
+
+with left_column:
+
+    search_query = st.text_input("Search by fighter name:", "")
+
+    num_rows_to_display = st.selectbox("Number of rows to display:", options=[10, 20, 30, 40, 50, 100], index=2)
+
+with right_column :
+        
+    mode = st.selectbox("Viewing Options", options=["Rankings", "Compare Fighters"], index=0) 
 
 st.markdown("""<br><br>""", unsafe_allow_html=True)
 
 reset_button = st.button("Reset All Multipliers")
+
+
 
 def reset_multipliers():
     for key in st.session_state.keys():
@@ -39,7 +49,7 @@ if reset_button:
     reset_multipliers()
 
 st.markdown("""<br><br>""", unsafe_allow_html=True)
-
+st.markdown("""<br><br>""", unsafe_allow_html=True)
 
 if search_query:
     df_filtered = df[df['name'].str.contains(search_query, case=False)]
@@ -66,7 +76,15 @@ def load_data(search_query, num_rows_to_display):
         df_filtered = df.head(num_rows_to_display)
     return df_filtered
 
-def render_row(row):
+def load_search_data(search_query, num_rows_to_display):
+    df = pd.read_csv("final_values.csv")
+    if search_query:
+        df_filtered = df[df['name'].str.contains(search_query, case=False)]
+    else :
+        df_filtered = df.head(num_rows_to_display)
+    return df_filtered
+
+def render_row(row, column_id):
     value_key = f"value_{row['name'].replace(' ', '_')}"
     multiplier_key = f"multiplier_{row['name'].replace(' ', '_')}"
 
@@ -88,7 +106,8 @@ def render_row(row):
     for i, multiplier in enumerate(multipliers):
         with button_cols[i]:
             st.markdown(f"<div style='margin-top: 10px;'></div>", unsafe_allow_html=True)
-            if st.button(f'{multiplier}x', key=f'{multiplier}_{row.name}'):
+            button_key = f'{multiplier}_{row["name"].replace(" ", "_")}_{column_id}' # Correct key format
+            if st.button(f'{multiplier}x', key=button_key):
                 new_value = round(row['Value'] * multiplier)
                 st.session_state[value_key] = new_value
                 st.session_state[multiplier_key] = multiplier
@@ -102,7 +121,76 @@ def render_row(row):
         chart_data.columns = ['Category', 'Value']
         st.bar_chart(chart_data.set_index('Category'))
 
-df_filtered = load_data(search_query, num_rows_to_display)
+def render_row_compare(row, column_id):
+    value_key = f"value_{row['name'].replace(' ', '_')}"
+    multiplier_key = f"multiplier_{row['name'].replace(' ', '_')}"
 
-for _, row in df_filtered.iterrows():
-    render_row(row)
+    if value_key not in st.session_state:
+        st.session_state[value_key] = round(row['Value'] * 1.2)
+    
+    if multiplier_key not in st.session_state:
+        st.session_state[multiplier_key] = 1.2
+
+    col1, col2 = st.columns([3, 5])
+
+    with col1:
+        st.markdown(f"## {row['name']}")
+
+    with col2:
+        color = multiplier_colors[st.session_state[multiplier_key]]
+        value_placeholder = st.markdown(f"<h2 style='color: {color};'>{st.session_state[value_key]}</h2>", unsafe_allow_html=True)
+
+    button_cols = st.columns(len(multipliers))
+
+    for i, multiplier in enumerate(multipliers):
+        with button_cols[i]:  # This selects each column one by one
+            st.markdown(f"<div style='margin-top: 10px;'></div>", unsafe_allow_html=True)
+            button_key = f'{multiplier}_{row["name"].replace(" ", "_")}_{column_id}' # Correct key format
+            if st.button(f'{multiplier}x', key=button_key):
+                new_value = round(row['Value'] * multiplier)
+                st.session_state[value_key] = new_value
+                st.session_state[multiplier_key] = multiplier
+                color = multiplier_colors[multiplier]
+                value_placeholder.markdown(f"<h2 style='color: {color};'>{st.session_state[value_key]}</h2>", unsafe_allow_html=True)
+
+    with st.expander("More Data"):
+        data_for_chart = row.drop(labels=['name', 'Value'])
+        chart_data = pd.DataFrame(data_for_chart)
+        chart_data = chart_data.rename(columns={row.name: 'Value'}).reset_index()
+        chart_data.columns = ['Category', 'Value']
+        st.bar_chart(chart_data.set_index('Category'))
+
+def load_all_fighter_names():
+    df = pd.read_csv("final_values.csv")
+    return df['name'].sort_values().unique()
+
+fighter_names = load_all_fighter_names()
+
+if mode == "Rankings" :
+    df_filtered = load_data(search_query, num_rows_to_display)
+    for _, row in df_filtered.iterrows():
+        render_row(row, "none")
+
+elif mode == "Compare Fighters" :
+
+    left_column, right_column = st.columns([2, 2])
+
+    with left_column:
+        fighter1 = st.selectbox("Select Fighter 1", options=fighter_names, index=11, format_func=lambda x: x if x else "Type to search...")
+
+        if not fighter1 == "" :
+            df_filtered = load_search_data(fighter1, num_rows_to_display)
+            for _, row in df_filtered.iterrows():
+                render_row_compare(row, "left")
+
+    
+    with right_column:
+        fighter2 = st.selectbox("Select Fighter 2", options=fighter_names, index=10, format_func=lambda x: x if x else "Type to search...")
+
+        if not fighter2 == "" :
+            df_filtered = load_search_data(fighter2, num_rows_to_display)
+            for _, row in df_filtered.iterrows():
+                render_row_compare(row, "right")
+
+
+
